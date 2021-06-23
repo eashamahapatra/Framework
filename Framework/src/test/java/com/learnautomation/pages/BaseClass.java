3601cce9c25501481a434f2ae1113d66ffc8dfd0
0289F8744 <0289F8744@9.199.56.52>
2021-06-23 21:10:44 +0530

    adding few new lines

--- Framework/src/test/java/com/learnautomation/pages/BaseClass.java
+++ Framework/src/test/java/com/learnautomation/pages/BaseClass.java
@@ -9,6 +9,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
@@ -44,12 +45,16 @@ public class BaseClass {
 
 	}
 
+	@Parameters({"browser","URLtoBeTested"})
+	
 	@BeforeClass
-	public void setup() {
+	public void setup(String browser, String url) {
 		Reporter.log("Trying to start Browser and getting application ready", true);
 
-		driver = BrowserFactory.startApplication(driver, config.getBrowser(), config.getStagingURL());
+		//driver = BrowserFactory.startApplication(driver, config.getBrowser(), config.getStagingURL());
 
+		driver = BrowserFactory.startApplication(driver, browser, url);
+		
 		Reporter.log("Browser and Application is up and running", true);
 	}
 
